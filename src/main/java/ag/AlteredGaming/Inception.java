57ab02d033e45b83f85e51d8130e5ad0b3f9d713
Michael Fabian Dirks <xaymar@levelnull.de>
2012-04-06 15:41:58 +0200

    Updated pom.xml to support all needed cases. Prepared for Jenkins aswell. Added default configuration YAML files. Fixed dependency not working.

---
+++ src/main/java/ag/AlteredGaming/Inception.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ag.AlteredGaming;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Xaymar
+ */
+public class Inception extends JavaPlugin {
+
+    @Override
+    public void onDisable(){
+	
+    }
+
+    @Override
+    public void onEnable(){
+	
+    }
+
+    @Override
+    public void saveDefaultConfig(){
+	
+    }
+    
+    
+}
