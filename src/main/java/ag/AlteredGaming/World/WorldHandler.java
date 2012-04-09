f8bf6bbb8275a6ec780b6217513c04f384cb7876
Michael Fabian Dirks <xaymar@levelnull.de>
2012-04-09 17:47:34 +0200

    Update to version 0.0.4-Alpha. Changelog: - Implemented better handling of configuration values. No longer reads on runtime. Fixes #9 - Implemented preservation of FallDistance and Velocity. Closes #10 Known caveats: - Teleporting will not empty out space around the target point anymore. I need to figure out another way of detecting if the entity can teleport or not. For some reason players don't always end up in the targeted block. - Permissions not yet added.

--- src/main/java/ag/AlteredGaming/World/WorldHandler.java
+++ src/main/java/ag/AlteredGaming/World/WorldHandler.java
@@ -4,14 +4,16 @@ import ag.AlteredGaming.Inception;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.logging.Level;
-import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.util.Vector;
 
 /**
  *
@@ -28,11 +30,17 @@ public class WorldHandler {
     private int intUpperOverlap;
     private int intUpperTeleport;
     private int intUpperTeleportTarget;
+    private boolean bolUpperPreserveEntityVelocity;
+    private boolean bolUpperPreserveEntityFallDistance;
+    private EnumMap<EntityType, Boolean> ohmUpperEntityFilter;
     private World objLowerWorld;
     private int intLowerOverlapStart;
     private int intLowerOverlap;
     private int intLowerTeleport;
     private int intLowerTeleportTarget;
+    private boolean bolLowerPreserveEntityVelocity;
+    private boolean bolLowerPreserveEntityFallDistance;
+    private EnumMap<EntityType, Boolean> ohmLowerEntityFilter;
     private WorldHandlerRunnable objWorldHandlerRunnable;
     private int intWorldHandlerRunnableTask;
 
@@ -44,7 +52,6 @@ public class WorldHandler {
 
         //Check if we need to generate default files and generate them
         saveDefaultConfig();
-
         loadConfig();
 
         //Create per-tick check if an Entity is in the teleport area
@@ -71,11 +78,24 @@ public class WorldHandler {
             intUpperOverlap = objWorldConfig.getInt("upper.overlap", 0);
             intUpperTeleport = objWorldConfig.getInt("upper.teleport", 255);
             intUpperTeleportTarget = objWorldConfig.getInt("upper.teleporttarget", 0);
+            bolUpperPreserveEntityVelocity = objWorldConfig.getBoolean("upper.preserveentityvelocity", true);
+            bolUpperPreserveEntityFallDistance = objWorldConfig.getBoolean("upper.preserveentityfalldistance", true);
+            ohmUpperEntityFilter = new EnumMap<EntityType, Boolean>(EntityType.class);
+            for (EntityType et : EntityType.values()) {
+                ohmUpperEntityFilter.put(et, objWorldConfig.getBoolean("upper.entityfilter." + et.getName(), false));
+            }
             objLowerWorld = objPlugin.getServer().getWorld(objWorldConfig.getString("lower.world", ""));
             intLowerOverlapStart = objWorldConfig.getInt("lower.overlapstart", 255);
             intLowerOverlap = objWorldConfig.getInt("lower.overlap", 0);
             intLowerTeleport = objWorldConfig.getInt("lower.teleport", 0);
             intLowerTeleportTarget = objWorldConfig.getInt("lower.teleporttarget", 255);
+            bolLowerPreserveEntityVelocity = objWorldConfig.getBoolean("lower.preserveentityvelocity", true);
+            bolLowerPreserveEntityFallDistance = objWorldConfig.getBoolean("lower.preserveentityfalldistance", true);
+            ohmLowerEntityFilter = new EnumMap<EntityType, Boolean>(EntityType.class);
+            for (EntityType et : EntityType.values()) {
+                ohmLowerEntityFilter.put(et, objWorldConfig.getBoolean("lower.entityfilter." + et.getName(), false));
+            }
+
         } catch (FileNotFoundException ex) {
             objPlugin.getLogger().log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -94,39 +114,77 @@ public class WorldHandler {
     }
 
     public void tickEntityMoved() {
+        Vector _EntityVelocity = null;
+        float _EntityDistanceFallen = 0.0f;
         for (Entity ent : objWorld.getEntities()) {
             if (objUpperWorld != null) {
-                if (objWorldConfig.getBoolean("upper.entityfilter."+ent.getType().getName())) {
+                if (ohmUpperEntityFilter.get(ent.getType()) == true) {
                     continue;
                 }
                 if (ent.getLocation().getY() >= intUpperTeleport) {
+                    if (bolUpperPreserveEntityVelocity) {
+                        _EntityVelocity = ent.getVelocity();
+                    }
+                    if (bolUpperPreserveEntityFallDistance) {
+                        _EntityDistanceFallen = ent.getFallDistance();
+                    }
+
                     Location _UpperWorldExit = new Location(objUpperWorld,
                                                             ent.getLocation().getX(),
                                                             intUpperTeleportTarget - (ent.getLocation().getY() - intUpperTeleport),
                                                             ent.getLocation().getZ());
                     _UpperWorldExit.setPitch(ent.getLocation().getPitch());
                     _UpperWorldExit.setYaw(ent.getLocation().getYaw());
-                    //Make space for the entity to stand in.
-                    objUpperWorld.getBlockAt(_UpperWorldExit).setType(Material.AIR);
-                    objUpperWorld.getBlockAt(_UpperWorldExit.add(0, 1, 0)).setType(Material.AIR);
+                    /*
+                     * Figure out a better way to do this! TODO
+                     * //Make space for the entity to stand in.
+                     * objUpperWorld.getBlockAt(_UpperWorldExit).setType(Material.AIR);
+                     * objUpperWorld.getBlockAt(_UpperWorldExit.add(0, 1,
+                     * 0)).setType(Material.AIR);
+                     */
                     ent.teleport(_UpperWorldExit);
+
+                    if (bolUpperPreserveEntityVelocity) {
+                        ent.setVelocity(_EntityVelocity);
+                    }
+                    if (bolUpperPreserveEntityFallDistance) {
+                        ent.setFallDistance(_EntityDistanceFallen);
+                    }
                 }
             }
             if (objLowerWorld != null) {
-                if (objWorldConfig.getBoolean("lower.entityfilter."+ent.getType().getName(), false)) {
+                if (ohmLowerEntityFilter.get(ent.getType()) == true) {
                     continue;
                 }
                 if (ent.getLocation().getY() <= intLowerTeleport) {
+                    if (bolLowerPreserveEntityVelocity) {
+                        _EntityVelocity = ent.getVelocity();
+                    }
+                    if (bolLowerPreserveEntityFallDistance) {
+                        _EntityDistanceFallen = ent.getFallDistance();
+                    }
+
                     Location _LowerWorldExit = new Location(objLowerWorld,
                                                             ent.getLocation().getX(),
                                                             intLowerTeleportTarget + (ent.getLocation().getY() - intLowerTeleport),
                                                             ent.getLocation().getZ());
                     _LowerWorldExit.setPitch(ent.getLocation().getPitch());
                     _LowerWorldExit.setYaw(ent.getLocation().getYaw());
-                    //Make space for the entity to stand in.
-                    objLowerWorld.getBlockAt(_LowerWorldExit).setType(Material.AIR);
-                    objLowerWorld.getBlockAt(_LowerWorldExit.add(0, 1, 0)).setType(Material.AIR);
+                    /*
+                     * Figure out a better way to do this! TODO
+                     * //Make space for the entity to stand in.
+                     * objLowerWorld.getBlockAt(_LowerWorldExit).setType(Material.AIR);
+                     * objLowerWorld.getBlockAt(_LowerWorldExit.add(0, 1,
+                     * 0)).setType(Material.AIR);
+                     */
                     ent.teleport(_LowerWorldExit);
+
+                    if (bolLowerPreserveEntityVelocity) {
+                        ent.setVelocity(_EntityVelocity);
+                    }
+                    if (bolLowerPreserveEntityFallDistance) {
+                        ent.setFallDistance(_EntityDistanceFallen);
+                    }
                 }
             }
         }
